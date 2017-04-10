e92c6451e1e281e016ee296e73b22ea2cbf3c101
郑俊鑫 <18810544291@163.com>
2017-04-10 17:42:38 +0800

    init config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 exports.WECHAT_CONFIG = {
-    token: 'wechat',
-    appid: 'wxb71708e8fc08e734',
-    encodingAESKey: 'RSmS09Xf7H2MzxhoGehEkeqwbCUjJu1wjsuPmQk8P7V',
+    token: your_token,
+    appid: your_appid,
+    encodingAESKey: your_encodingAESKey,
 }
